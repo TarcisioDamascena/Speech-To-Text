aa68ce01ae8ecf59875b0791148f39b8cacfc703
TarcisioDamascena <damascenatarcisio@gmail.com>
2021-06-09 15:33:30 -0300

    arquivos adicionados a pasta principal
    biblioteca pyaudio

---
+++ main.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Diga algo: ")
+    audio = r.listen(source)
+
+    texto = r.recognize_google(audio, language='pt-BR')
+
+    try:
+        print("Você disse: " + texto)
+    except:
+        print("Não entendi nada!")
